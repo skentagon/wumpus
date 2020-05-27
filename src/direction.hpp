37cc197dcd2ad6978c82a410a3005f82839a7bca
skentagon <kentaro23313@zeus.eonet.ne.jp>
2020-05-27 09:16:45 +0900

    edit

--- src/direction.hpp
+++ src/direction.hpp
@@ -7,56 +7,58 @@
 #include <iostream>
 #include "pos.hpp"
 
-namespace wumpus::util {
+namespace wumpus {
+  namespace util {
 
-  class Direction {
-    public:
-      enum DirectionType {
-        north = 0,
-        south,
-        east,
-        west
-      };
-      Direction() = default;
-      Direction( DirectionType type ) : type(type) {}
-      Direction( int p ) : type(static_cast<DirectionType>(p)) {}
-      Direction( char ch ){
-        switch(ch){
-          case 'w':
-            type = DirectionType::north;
-            break;
-          case 'a':
-            type = DirectionType::east;
-            break;
-          case 's':
-            type = DirectionType::south;
-            break;
-          case 'd':
-            type = DirectionType::west;
-            break;
-          default:
-            std::cout << ch << std::endl;
-            throw "unexpected char";
-            break;
+    class Direction {
+      public:
+        enum DirectionType {
+          north = 0,
+          south,
+          east,
+          west
         };
-      };
-      util::Pos getDirection() const {
-        switch(type){
-          case DirectionType::north:
-            return util::Pos(-1,0);
-          case DirectionType::west:
-            return util::Pos(0,1);
-          case DirectionType::south:
-            return util::Pos(1,0);
-          case DirectionType::east:
-            return util::Pos(0,-1);
-          default:
-            throw "unexpected type";
+        Direction() = default;
+        Direction( DirectionType type ) : type(type) {}
+        Direction( int p ) : type(static_cast<DirectionType>(p)) {}
+        Direction( char ch ){
+          switch(ch){
+            case 'w':
+              type = DirectionType::north;
+              break;
+            case 'a':
+              type = DirectionType::east;
+              break;
+            case 's':
+              type = DirectionType::south;
+              break;
+            case 'd':
+              type = DirectionType::west;
+              break;
+            default:
+              std::cout << ch << std::endl;
+              throw "unexpected char";
+              break;
+          };
         };
-      }
-    private:
-      DirectionType type;
-  };
+        util::Pos getDirection() const {
+          switch(type){
+            case DirectionType::north:
+              return util::Pos(-1,0);
+            case DirectionType::west:
+              return util::Pos(0,1);
+            case DirectionType::south:
+              return util::Pos(1,0);
+            case DirectionType::east:
+              return util::Pos(0,-1);
+            default:
+              throw "unexpected type";
+          };
+        }
+      private:
+        DirectionType type;
+    };
+  }
 }
 
 #endif
--- src/pos.hpp
+++ src/pos.hpp
@@ -3,24 +3,26 @@
 #define SKENTAGON_WUMPUS_POS_HEADER_INCLUDED
 #pragma once
 
-namespace wumpus::util {
-  class Pos {
-    public:
-      Pos() = default;
-      Pos( int x, int y ) : x(x), y(y) {}
-      Pos operator+( const Pos& pos ){
-        Pos t(*this);
-        t.x += pos.x;
-        t.y += pos.y;
-        return t;
-      }
-      bool operator==( const Pos& p ){
-        return x == p.x && y == p.y;
-      }
-      bool operator!=( const Pos& p ){ return !operator==(p); }
-      int x;
-      int y;
-  };
+namespace wumpus {
+  namespace util {
+    class Pos {
+      public:
+        Pos() = default;
+        Pos( int x, int y ) : x(x), y(y) {}
+        Pos operator+( const Pos& pos ){
+          Pos t(*this);
+          t.x += pos.x;
+          t.y += pos.y;
+          return t;
+        }
+        bool operator==( const Pos& p ){
+          return x == p.x && y == p.y;
+        }
+        bool operator!=( const Pos& p ){ return !operator==(p); }
+        int x;
+        int y;
+    };
+  }
 }
 
 #endif
